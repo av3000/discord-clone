53b739b744b861f8fb73f21edeee72d35fca9b5d
av3000 <alanas.vaiciulis@gmail.com>
2024-07-07 20:46:44 +0300

    feat(server update): implement name and image Url update, create edit modal & patch endpoint

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { HttpResponseMessages, HttpResponses } from "@/lib/utils";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) {
+      return new NextResponse(
+        HttpResponseMessages[HttpResponses.UNAUTHORIZED],
+        { status: HttpResponses.UNAUTHORIZED }
+      );
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_PATCH", error);
+    return new NextResponse(
+      HttpResponseMessages[HttpResponses.INTERNAL_SERVER_ERROR],
+      { status: HttpResponses.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
