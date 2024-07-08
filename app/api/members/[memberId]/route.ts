2b44ec4f4bf05709e4f193da5d981c61f2a2d6e7
av3000 <alanas.vaiciulis@gmail.com>
2024-07-08 15:42:59 +0300

    feat(members modal): create members modal, add avatars ui components, create patch server members endpoint route

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,76 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { HttpResponseMessages, HttpResponses } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse(
+        HttpResponseMessages[HttpResponses.UNAUTHORIZED],
+        { status: HttpResponses.UNAUTHORIZED }
+      );
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID Missing", {
+        status: HttpResponses.BAD_REQUEST,
+      });
+    }
+
+    if (!params.memberId) {
+      return new NextResponse("Member ID Missing", {
+        status: HttpResponses.BAD_REQUEST,
+      });
+    }
+
+    const updatedServer = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberId,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(updatedServer);
+  } catch (error) {
+    console.log("[MEMBERS_ID_PATCH]", error);
+    return new NextResponse(
+      HttpResponseMessages[HttpResponses.INTERNAL_SERVER_ERROR],
+      { status: HttpResponses.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -57,8 +57,11 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
-            Manage Memebers <Users className="h-4 w-4 ml-auto" />
+          <DropdownMenuItem
+            onClick={() => onOpen(ModalTypeEnum.ManageMembers, { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Manage Members <Users className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isModerator && (
