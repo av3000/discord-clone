7e48c300d3e872146b99e64f2e1ea142173b35e3
av3000 <alanas.vaiciulis@gmail.com>
2024-07-16 22:23:21 +0300

    feat(view channel): create server page and reroute on visit to first/general channel, handle event propagation for channel action vs channel visit

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -7,7 +7,11 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tool";
-import { ModalTypeEnum, useModalStore } from "@/hooks/use-modal-store";
+import {
+  ModalType,
+  ModalTypeEnum,
+  useModalStore,
+} from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -31,9 +35,18 @@ export const ServerChannel = ({
   const params = useParams();
   const router = useRouter();
 
+  const openChannelPage = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onChannelAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
+
   return (
     <button
-      onClick={() => {}}
+      onClick={openChannelPage}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1"
       )}
@@ -52,17 +65,13 @@ export const ServerChannel = ({
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
             <Edit
-              onClick={() =>
-                onOpen(ModalTypeEnum.EditChannel, { server, channel })
-              }
+              onClick={(e) => onChannelAction(e, ModalTypeEnum.EditChannel)}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() =>
-                onOpen(ModalTypeEnum.DeleteChannel, { server, channel })
-              }
+              onClick={(e) => onChannelAction(e, ModalTypeEnum.DeleteChannel)}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -19,8 +19,13 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 
   const roleIcon = roleIconMap[member.role];
 
+  const onConversationClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
   return (
     <button
+      onClick={onConversationClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/2 dark:bg-zinc-700"
@@ -33,11 +38,10 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
       <p
         className={cn(
           "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-          params?.channelId === member.id &&
+          params?.memberId === member.id &&
             "text-primary dark:text-zinc-200 dark:group-hover:text-white"
         )}
       >
-        {/* channelId === member.id ??? logic seems odd */}
         {member.profile.name}
       </p>
       {roleIcon}
