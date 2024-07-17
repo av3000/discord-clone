5c8ee1773187252a66da88127b20c196c46f09e5
av3000 <alanas.vaiciulis@gmail.com>
2024-07-17 14:26:12 +0300

    feat(chat header): add chat type, create mobile size toggle, create chat header component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,51 @@
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatHeaderType } from "@/types";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type={ChatHeaderType.Channel}
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
--- types.ts
+++ types.ts
@@ -9,4 +9,9 @@ export enum SectionTypeEnum {
   Members = "Members",
 }
 
+export enum ChatHeaderType {
+  Channel = "Channel",
+  Conversation = "Conversation",
+}
+
 export type SectionType = SectionTypeEnum.Channels | SectionTypeEnum.Members;
